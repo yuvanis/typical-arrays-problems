a0f8941e0ce6c7f5f9fe740d394f242560f1c4d1
yuvanis <75557443+yuvanis@users.noreply.github.com>
2021-06-07 22:07:41 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else {
+    let minValue = array[0];
+    for (let i = 0; i < array.length; i++){
+      if (array[i] < minValue){
+          minValue = array[i];
+      }
+    }
+    return minValue;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else {
+    let maxValue = array[0];
+    for (let i = 0; i < array.length; i++){
+      if (array[i] > maxValue){
+          maxValue = array[i];
+      }
+    }
+    return maxValue;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  else {
+    let sum = 0;
+    let avgValue = array[0];
+    for (let i = 0; i < array.length; i++){
+
+          sum += array[i];
+          avgValue = sum / array.length;
+    }
+    return avgValue;
+  }
 }
